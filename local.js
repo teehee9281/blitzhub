3d12ea335be50aa0221d69c5595af377de272c4e
teehee9281 <116509230+teehee9281@users.noreply.github.com>
2024-10-12 14:44:25 -0400

    beacon

--- local.js
+++ local.js
@@ -238,7 +238,7 @@ function openSlope() {
 
 
 function openBacon() {
-  openWindow("https://game.enjoy4fun.com/baconmaydie/v2/index.html?uid=cb7u1g6rsfesr7vv63d0&channel-id=3487216655", 'Google Docs', 'CloakIMGs/docs.ico');
+  openWindow("https://www.spiele-umsonst.de/azad/downloads/html5games/action/baconmaydie/", 'Google Docs', 'CloakIMGs/docs.ico');
 }
 
 
